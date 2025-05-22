88402f349b035e8c41ec70cbdf27abf8c9b4cf20
Brandon Kammerdiener <kammerdienerb@MacBook-Pro-330.local>
2025-05-22 09:28:50 -0400

    init

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+PCRE2_FLAGS=""
+if which pcre2-config > /dev/null && ! [[ $(pcre2-config --version) < "10.36" ]]; then
+    PCRE2_FLAGS="$(pcre2-config --cflags-posix --libs-posix) -DYED_SYNTAX_USE_PCRE2"
+fi
+
+gcc -o julie.so julie.c $(yed --print-cflags --print-ldflags) ${PCRE2_FLAGS}
